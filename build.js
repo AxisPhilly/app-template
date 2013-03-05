30de6c884bfc0a711babf662b6341e6775a6956b
Casey Thomas <cpthomas@gmail.com>
2013-03-04 22:02:37 -0500

    Remove space

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ var server  = require('./server.js'),
 
 server.routes.get.forEach(function(route, i, arr) {
   request.get('http://0.0.0.0:3000' + route.path, function(err, res, body) {
-    var fileName = (route.path === '/') ? '/index.html' : route.path.split('/')[1] + '.html'; 
+    var fileName = (route.path === '/') ? '/index.html' : route.path.split('/')[1] + '.html';
 
     fs.writeFile('www/' + fileName, body, function(err) {
       if (err) throw err;
