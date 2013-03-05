1349995d200adf3a8b4bc7b93f079447cc2d7c79
Casey Thomas <cpthomas@gmail.com>
2013-03-05 17:54:38 -0500

    Add build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,8 +42,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', '');
+  grunt.registerTask('default', ''); // Intentionally left blank in the interest of being explicit
 
-  grunt.registerTask('default', ['jshint', 'uglify', 'sass', 'shell']);
+  grunt.registerTask('build', ['jshint', 'uglify', 'sass', 'shell']);
 
 };
