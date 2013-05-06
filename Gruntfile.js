e70088efc5470ad083e91629dda4eab43a19f0ae
Casey Thomas <cpthomas@gmail.com>
2013-05-06 15:37:47 -0400

    Fixing up deploy and build process. Still needs a few more tweaks

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,11 +31,18 @@ module.exports = function(grunt) {
         }
       }
     },
+    concat: {
+      dist: {
+        src: ['css/lib/*'],
+        dest: 'www/css/libraries.css'
+      }
+    },
     copy: {
       main: {
         files: [
           {expand: true, src: ['css/lib/*'], dest: 'www/'},
-          {expand: true, src: ['img/lib/**'], dest: 'www/'}
+          {expand: true, src: ['img/lib/**'], dest: 'www/'},
+          {expand: true, src: ['img/**'], dest: 'www/'}
         ]
       }
     },
@@ -65,6 +72,10 @@ module.exports = function(grunt) {
         {
           src: 'www/data/*',
           dest: '<%= pkg.name %>/data'
+        },
+        {
+          src: 'www/img/**',
+          dest: '<%= pkg.name %>/img'
         }
       ]
     }
@@ -74,12 +85,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-s3');
 
   grunt.registerTask('default', ''); // Intentionally left blank in the interest of being explicit
 
-  grunt.registerTask('build', ['jshint', 'uglify', 'sass', 'copy', 'shell']);
+  grunt.registerTask('build', ['jshint', 'uglify', 'sass', 'concat', 'copy', 'shell']);
   grunt.registerTask('deploy', ['s3']);
 
 };
