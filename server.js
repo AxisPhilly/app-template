1810e4dd74cb32d8ebc50848ba4e4fcbf6f16aaf
Casey Thomas <cpthomas@gmail.com>
2013-07-02 14:10:16 -0400

    Pass version to templates

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var sass = require('node-sass');
+var pkg = require('./package.json');
 
 var app = express();
 
@@ -24,14 +25,16 @@ app.use('/data', express.static(__dirname + '/data'));
 app.get('/', function(req, res){
   res.render('index', {
     title: 'App Title | AxisPhilly',
-    env: app.settings.env
+    env: app.settings.env,
+    version: pkg.version
   });
 });
 
 app.get('/embed', function(req, res){
   res.render('embed', {
     title: 'App Title | AxisPhilly',
-    env: app.settings.env
+    env: app.settings.env,
+    version: pkg.version
   });
 });
 
